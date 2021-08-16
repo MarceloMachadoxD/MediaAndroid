c462403af0129ad942c91e75942386b766b526c5
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-08-16 19:44:44 -0300

    1st simple app

---
+++ app/src/main/java/com/github/marcelomachadoxd/hoje/MainActivity.java
@@ -0,0 +1,43 @@
+package com.github.marcelomachadoxd.hoje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText n1, n2, n3;
+    TextView mediaResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        n1 = (EditText) findViewById(R.id.num1);
+        n2 = (EditText) findViewById(R.id.num2);
+        n3 = (EditText) findViewById(R.id.num3);
+
+
+        mediaResultado = (TextView) findViewById(R.id.media);
+
+    }
+
+    public void calcular(View view) {
+        Double a1 = Double.parseDouble(String.valueOf(n1.getText()));
+        Double a2 = Double.parseDouble(String.valueOf(n2.getText()));
+        Double a3 = Double.parseDouble(String.valueOf(n3.getText()));
+
+        Double media = (a1 + a2 + a3) / 3;
+
+        mediaResultado.setText(media.toString());
+
+
+
+    }
+}
